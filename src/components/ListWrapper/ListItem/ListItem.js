1104f078a4ea2e23886fc2f7952f32521fd6eab6
Radosław Grzymała <radoslaw.grzymala@sensilabs.pl>
2019-11-14 13:08:10 +0100

    listing

--- src/components/ListWrapper/ListItem/ListItem.js
+++ src/components/ListWrapper/ListItem/ListItem.js
@@ -1,28 +1,16 @@
 import React from 'react';
 import './ListItem.css';
-import { twitterAccounts } from '../../../data/twitterAccounts'
 
-const ListItem = () => (
-    <>
-        {
-            twitterAccounts.map(
-                item => {
-                    return (
-                        <li key={item.name} className="listItem__wrapper">
-                            <img className="listItem__image" src={item.image} alt="dan" />
-                            <div className="listItem_content">
-                                <h2 className="listItem__name">{item.name}</h2>
-                                <p className="listItem__description">{item.description}</p>
-                                <button className="listItem__button">visit twitter page</button>
-                            </div>
-                        </li>
+const ListItem = ({ name, image, description }) => (
+    <li key={name} className="listItem__wrapper">
+        <img className="listItem__image" src={image} alt="avatar" />
+        <div className="listItem_content">
+            <h2 className="listItem__name">{name}</h2>
+            <p className="listItem__description">{description}</p>
+            <button className="listItem__button">visit twitter page</button>
+        </div>
+    </li>
 
-                    )
-                }
-
-            )
-        }
-    </>
 );
 
 export default ListItem;
--- src/components/ListWrapper/ListWrapper.js
+++ src/components/ListWrapper/ListWrapper.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ListItem from './ListItem/ListItem';
 import './ListWrapper.css';
+import { twitterAccounts } from '../../data/twitterAccounts'
+
+const item = (item) => {
+    return (
+        <ListItem
+            key={item.name}
+            name={item.name}
+            image={item.image}
+            description={item.description}
+        />
+    )
+}
 
 const ListWrapper = () => (
     <ul className="listWrapper">
-        <ListItem/>
+        {
+            twitterAccounts.map(item)
+        }
     </ul>
 )
-
 
 export default ListWrapper;
